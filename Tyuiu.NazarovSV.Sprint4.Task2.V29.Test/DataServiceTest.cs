b7398eaf6824c0d3cc4ace87322ab7228d53b7ab
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-17 16:48:20 +0500

    Task2

---
+++ Tyuiu.NazarovSV.Sprint4.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NazarovSV.Sprint4.Task2.V29.Lib;
+namespace Tyuiu.NazarovSV.Sprint4.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numbers = { 5, 1, 7, 3, 1, 2, 1, 7, 2, 1, 6 };
+            int res = ds.Calculate(numbers);
+            int next = 24;
+            Assert.AreEqual(next, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint4.Task2.V29/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NazarovSV.Sprint4.Task2.V29.Lib;
+namespace Tyuiu.NazarovSV.Sprint4.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                numbers[i] = rnd.Next(1, 8);
+            }
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(numbers[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("РЕЗУЛЬТАТ: ");
+            int res = ds.Calculate(numbers);
+            Console.WriteLine("Произведение чётных чисел массива = " + res);
+            Console.ReadKey();
+        }
+    }
+}

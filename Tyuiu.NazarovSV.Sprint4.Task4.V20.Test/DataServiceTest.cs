70ba1491a24e23a6a771a7d6cdc412db0d001d6f
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-11-18 16:48:32 +0500

    Task4

---
+++ Tyuiu.NazarovSV.Sprint4.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NazarovSV.Sprint4.Task4.V20.Lib;
+namespace Tyuiu.NazarovSV.Sprint4.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { 4, 5, 5, 6, 4 }, { 7, 8, 4, 7, 5 }, { 5, 6, 5, 8, 5 }, { 7, 5, 8, 7, 8 }, { 4, 7, 7, 8, 8 } };
+            int[,] next = new int[5, 5] { { 1, 5, 5, 1, 1 }, { 7, 1, 1, 7, 5 }, { 5, 1, 5, 1, 5 }, { 7, 5, 1, 7, 1 }, { 1, 7, 7, 1, 1 } };
+            int[,] res = ds.Calculate(mas2);
+            CollectionAssert.AreEqual(next, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint4.Task4.V20/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.NazarovSV.Sprint4.Task4.V20.Lib;
+namespace Tyuiu.NazarovSV.Sprint4.Task4.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int colums = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[rows, colums];
+            Console.WriteLine("**************************************************************************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"Введите {i}, {j} элемент массива: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("РЕЗУЛЬТАТ:            ");
+            int[,] res = ds.Calculate(matrix);
+            Console.WriteLine("Массив примет следующий вид - " + res);
+            Console.ReadKey();
+        }
+    }
+}
